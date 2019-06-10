ac35494bbb244093af3e84ec872259d4c01b9de8
marshallprotsman <47481879+MarshallProtsman@users.noreply.github.com>
2019-06-10 16:06:45 -0400

    DB real nice

--- db/schema.sql
+++ db/schema.sql
@@ -1,4 +1,4 @@
--- DROP DATABASE [IF EXISTS] pawnder_db;
+DROP DATABASE IF EXISTS pawnder_db;
 CREATE DATABASE pawnder_db;
 USE pawnder_db;
 
@@ -11,6 +11,20 @@ CREATE TABLE dogs
     other_dogs BOOLEAN DEFAULT true,
     kids BOOLEAN DEFAULT true,
     PRIMARY KEY (dog_id)
+);
+
+
+DROP TABLE IF EXISTS human_prefs;
+CREATE TABLE human_prefs
+(
+    prefs_id INT NOT NULL AUTO_INCREMENT,
+    yard BOOLEAN DEFAULT true,
+    other_dogs BOOLEAN DEFAULT true,
+    kids BOOLEAN DEFAULT true,
+    small BOOLEAN DEFAULT true,
+    medium BOOLEAN DEFAULT true,
+    large BOOLEAN DEFAULT true,
+    PRIMARY KEY (prefs_id)
 );
 
 DROP TABLE IF EXISTS humans;
@@ -32,14 +46,4 @@ CREATE TABLE humans
     FOREIGN KEY (dog_match_4) REFERENCES dogs(dog_id),
     FOREIGN KEY (dog_match_5) REFERENCES dogs(dog_id),
     FOREIGN KEY (prefs) REFERENCES human_prefs(prefs_id)
-);
-
-DROP TABLE IF EXISTS human_prefs;
-CREATE TABLE human_prefs
-(
-    prefs_id INT NOT NULL AUTO_INCREMENT,
-    yard BOOLEAN DEFAULT true,
-    other_dogs BOOLEAN DEFAULT true,
-    kids BOOLEAN DEFAULT true,
-    PRIMARY KEY (prefs_id)
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,8 +1,3 @@
-INSERT INTO humans (name, password, prefs) VALUES ('John', 'blink182', 1);
-INSERT INTO humans (name, password, prefs) VALUES ('Marshall', 'drink!c0ff33!', 2);
-INSERT INTO humans (name, password, prefs) VALUES ('Chaney', 'd0g54lyf3', 3);
-INSERT INTO humans (name, password, prefs) VALUES ('David', 'cod3r', 4);
-INSERT INTO humans (name, password, prefs) VALUES ('Stanley', 'ipromiseim18', 5);
 
 -- Template for adding dogs to a persons interests
 -- update humans set dog_match_1=1 WHERE id = 1;
@@ -16,8 +11,18 @@ INSERT INTO dogs (dog_name, yard, other_dogs, kids) VALUES ('Spot', true, true, 
 INSERT INTO dogs (dog_name, yard, other_dogs, kids) VALUES ('Lassie', false, false, false);
 INSERT INTO dogs (dog_name, yard, other_dogs, kids) VALUES ('StanMan', true, true, true);
 
-INSERT INTO human_prefs (yard, other_dogs, kids) VALUES (true, false, true);
-INSERT INTO human_prefs (yard, other_dogs, kids) VALUES (true, true, true);
-INSERT INTO human_prefs (yard, other_dogs, kids) VALUES (false, false, true);
-INSERT INTO human_prefs (yard, other_dogs, kids) VALUES (false, false, false);
-INSERT INTO human_prefs (yard, other_dogs, kids) VALUES (true, false, false);
+INSERT INTO human_prefs (yard, other_dogs, kids, small, medium, large) VALUES (false, false, true, false, true, true);
+INSERT INTO human_prefs (yard, other_dogs, kids, small, medium, large) VALUES (true, true, true, true, true, true);
+INSERT INTO human_prefs (yard, other_dogs, kids, small, medium, large) VALUES (false, false, false, true, false, false);
+INSERT INTO human_prefs (yard, other_dogs, kids, small, medium, large) VALUES (true, false, true, false, true, false);
+INSERT INTO human_prefs (yard, other_dogs, kids, small, medium, large) VALUES (true, false, false, false, false, true);
+
+INSERT INTO humans (name, password, prefs) VALUES ('John', 'blink182', 1);
+INSERT INTO humans (name, password, prefs) VALUES ('Marshall', 'drink!c0ff33!', 2);
+INSERT INTO humans (name, password, prefs) VALUES ('Chaney', 'd0g54lyf3', 3);
+INSERT INTO humans (name, password, prefs) VALUES ('David', 'cod3r', 4);
+INSERT INTO humans (name, password, prefs) VALUES ('Stanley', 'ipromiseim18', 5);
+
+SELECT * FROM dogs;
+SELECT * FROM human_prefs;
+SELECT * FROM humans;
