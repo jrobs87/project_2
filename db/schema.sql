afdb62489407c08950d51f0168523be2f377eb39
marshallprotsman <47481879+MarshallProtsman@users.noreply.github.com>
2019-06-14 10:18:44 -0400

    restructured 1NF compliant

--- db/schema.sql
+++ db/schema.sql
@@ -5,28 +5,50 @@ USE pawnder_db;
 DROP TABLE IF EXISTS dogs;
 CREATE TABLE dogs
 (
-    dog_name varchar(255) NOT NULL,
     dog_id INT NOT NULL AUTO_INCREMENT,
-    yard BOOLEAN DEFAULT true,
-    other_dogs BOOLEAN DEFAULT true,
-    kids BOOLEAN DEFAULT true,
-    size VARCHAR(6) NOT NULL,
+    dog_name varchar(255) NOT NULL,
     PRIMARY KEY (dog_id)
 );
 
+DROP TABLE IF EXISTS dogs_links;
+CREATE TABLE dogs_links
+(
+    dogs_links_id INT(4) AUTO_INCREMENT,
+    profile_url VARCHAR(255) NOT NULL,
+    profile_image VARCHAR(255) NOT NULL,
+    PRIMARY KEY (dogs_links_id)
+);
 
--- DROP TABLE IF EXISTS human_prefs;
--- CREATE TABLE human_prefs
--- (
---     prefs_id INT NOT NULL AUTO_INCREMENT,
---     yard BOOLEAN DEFAULT true,
---     other_dogs BOOLEAN DEFAULT true,
---     kids BOOLEAN DEFAULT true,
---     small BOOLEAN DEFAULT true,
---     medium BOOLEAN DEFAULT true,
---     large BOOLEAN DEFAULT true,
---     PRIMARY KEY (prefs_id)
--- );
+DROP TABLE IF EXISTS dogs_stats;
+CREATE TABLE dogs_stats
+(
+    FK_dog_id INT NOT NULL,
+    yard BOOLEAN DEFAULT true,
+    other_pets BOOLEAN DEFAULT true,
+    kids BOOLEAN DEFAULT true,
+    female BOOLEAN NOT NULL,
+    size VARCHAR(6) NOT NULL,
+    
+    FK_dogs_links_id INT NOT NULL,
+
+    FOREIGN KEY (FK_dog_id) REFERENCES dogs(dog_id),
+    FOREIGN KEY (FK_dogs_links_id) REFERENCES dogs_links(dogs_links_id)
+);
+
+
+DROP TABLE IF EXISTS human_prefs;
+CREATE TABLE human_prefs
+(
+    prefs_id INT NOT NULL AUTO_INCREMENT,
+    yard BOOLEAN DEFAULT true,
+    other_pets BOOLEAN DEFAULT false,
+    kids BOOLEAN DEFAULT true,
+    female BOOLEAN ,
+    small BOOLEAN DEFAULT true,
+    medium BOOLEAN DEFAULT true,
+    large BOOLEAN DEFAULT true,
+    PRIMARY KEY (prefs_id)
+);
 
 DROP TABLE IF EXISTS humans;
 CREATE TABLE humans
@@ -40,8 +62,8 @@ CREATE TABLE humans
 DROP TABLE IF EXISTS matches;
 CREATE TABLE matches
 (
-    human_id INT(4) NOT NULL,
-    dog_id INT(4) NOT NULL,
-    FOREIGN KEY (human_id) REFERENCES humans(id),
-    FOREIGN KEY (dog_id) REFERENCES dogs(dog_id)
+    FK_human_id INT(4) NOT NULL,
+    FK_dog_id INT(4) NOT NULL,
+    FOREIGN KEY (FK_human_id) REFERENCES humans(id),
+    FOREIGN KEY (FK_dog_id) REFERENCES dogs(dog_id)
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -5,27 +5,39 @@
 -- Template for adding a new person with a dog interest
 -- INSERT INTO humans (name, password, dog_match_1) VALUES ('', '', int);
 
-INSERT INTO dogs (dog_name, yard, other_dogs, kids, size) VALUES ('Bayley', true, true, true, 'medium');
-INSERT INTO dogs (dog_name, yard, other_dogs, kids, size) VALUES ('Emmett', false, false, true, 'large');
-INSERT INTO dogs (dog_name, yard, other_dogs, kids, size) VALUES ('Spot', true, true, true, 'small');
-INSERT INTO dogs (dog_name, yard, other_dogs, kids, size) VALUES ('Lassie', false, false, false, 'small');
-INSERT INTO dogs (dog_name, yard, other_dogs, kids, size) VALUES ('StanMan', true, true, true, 'medium');
-INSERT INTO dogs (dog_name, yard, other_dogs, kids, size) VALUES ('Stephen', true, true, true, 'medium');
-INSERT INTO dogs (dog_name, yard, other_dogs, kids, size) VALUES ('Twinkle', false, false, true, 'large');
-INSERT INTO dogs (dog_name, yard, other_dogs, kids, size) VALUES ('Tough', true, true, true, 'small');
-INSERT INTO dogs (dog_name, yard, other_dogs, kids, size) VALUES ('Dakota', false, false, false, 'small');
-INSERT INTO dogs (dog_name, yard, other_dogs, kids, size) VALUES ('Ariel', true, true, true, 'medium');
-INSERT INTO dogs (dog_name, yard, other_dogs, kids, size) VALUES ('Bark', true, true, true, 'medium');
-INSERT INTO dogs (dog_name, yard, other_dogs, kids, size) VALUES ('Max', false, false, true, 'large');
-INSERT INTO dogs (dog_name, yard, other_dogs, kids, size) VALUES ('Crosby', true, true, true, 'small');
-INSERT INTO dogs (dog_name, yard, other_dogs, kids, size) VALUES ('Matcha', false, false, false, 'small');
-INSERT INTO dogs (dog_name, yard, other_dogs, kids, size) VALUES ('Derek', true, true, true, 'medium');
+INSERT INTO dogs (dog_name) VALUES ('Bayley');
+INSERT INTO dogs (dog_name) VALUES ('Emmett');
+INSERT INTO dogs (dog_name) VALUES ('Spot');
+INSERT INTO dogs (dog_name) VALUES ('Lassie');
+INSERT INTO dogs (dog_name) VALUES ('StanMan');
+-- INSERT INTO dogs (dog_name, yard, other_pets, kids, female) VALUES ('Stephen', true, true, true, true, false, false, true);
+-- INSERT INTO dogs (dog_name, yard, other_pets, kids, female) VALUES ('Twinkle', false, false, true, true, true, false, false);
+-- INSERT INTO dogs (dog_name, yard, other_pets, kids, female) VALUES ('Tough', true, true, true, true, false, true, false);
+-- INSERT INTO dogs (dog_name, yard, other_pets, kids, female) VALUES ('Dakota', false, false, true, false, false, false, true);
+-- INSERT INTO dogs (dog_name, yard, other_pets, kids, female) VALUES ('Ariel', true, true, true, true, true, false, false);
+-- INSERT INTO dogs (dog_name, yard, other_pets, kids, female) VALUES ('Bark', true, true, true, true, false, true, false);
+-- INSERT INTO dogs (dog_name, yard, other_pets, kids, female) VALUES ('Max', false, false, true, true, false, false, true);
+-- INSERT INTO dogs (dog_name, yard, other_pets, kids, female) VALUES ('Crosby', true, true, true, true, true, false, false);
+-- INSERT INTO dogs (dog_name, yard, other_pets, kids, female) VALUES ('Matcha', false, false, true, false, false, true, false);
+-- INSERT INTO dogs (dog_name, yard, other_pets, kids, female) VALUES ('Derek', true, true, true, true, true, false, false);
 
--- INSERT INTO human_prefs (yard, other_dogs, kids, small, medium, large) VALUES (false, false, true, false, true, true);
--- INSERT INTO human_prefs (yard, other_dogs, kids, small, medium, large) VALUES (true, true, true, true, true, true);
--- INSERT INTO human_prefs (yard, other_dogs, kids, small, medium, large) VALUES (false, false, false, true, false, false);
--- INSERT INTO human_prefs (yard, other_dogs, kids, small, medium, large) VALUES (true, false, true, false, true, false);
--- INSERT INTO human_prefs (yard, other_dogs, kids, small, medium, large) VALUES (true, false, false, false, false, true);
+INSERT INTO dogs_links (profile_url, profile_image) VALUES ("https://www.petfinder.com/dog/hunter-44955346/ga/atlanta/atlanta-lab-rescue-ga508/", "https://dl5zpyw5k3jeb.cloudfront.net/photos/pets/44955346/1/?bust=1560494989&width=1080");
+INSERT INTO dogs_links (profile_url, profile_image) VALUES ("https://www.petfinder.com/dog/hunter-44955346/ga/atlanta/atlanta-lab-rescue-ga508/", "https://dl5zpyw5k3jeb.cloudfront.net/photos/pets/44955346/1/?bust=1560494989&width=1080");
+INSERT INTO dogs_links (profile_url, profile_image) VALUES ("https://www.petfinder.com/dog/hunter-44955346/ga/atlanta/atlanta-lab-rescue-ga508/", "https://dl5zpyw5k3jeb.cloudfront.net/photos/pets/44955346/1/?bust=1560494989&width=1080");
+INSERT INTO dogs_links (profile_url, profile_image) VALUES ("https://www.petfinder.com/dog/hunter-44955346/ga/atlanta/atlanta-lab-rescue-ga508/", "https://dl5zpyw5k3jeb.cloudfront.net/photos/pets/44955346/1/?bust=1560494989&width=1080");
+INSERT INTO dogs_links (profile_url, profile_image) VALUES ("https://www.petfinder.com/dog/hunter-44955346/ga/atlanta/atlanta-lab-rescue-ga508/", "https://dl5zpyw5k3jeb.cloudfront.net/photos/pets/44955346/1/?bust=1560494989&width=1080");
+
+INSERT INTO dogs_stats (FK_dog_id, yard, other_pets, kids, female, size, FK_dogs_links_id) VALUES (1, true, true, true, false, "small", 3);
+INSERT INTO dogs_stats (FK_dog_id, yard, other_pets, kids, female, size, FK_dogs_links_id) VALUES (3, true, false, true, false, "small", 4);
+INSERT INTO dogs_stats (FK_dog_id, yard, other_pets, kids, female, size, FK_dogs_links_id) VALUES (4, true, true, true, true, "medium", 1);
+INSERT INTO dogs_stats (FK_dog_id, yard, other_pets, kids, female, size, FK_dogs_links_id) VALUES (2, false, true, false, false, "small", 2);
+INSERT INTO dogs_stats (FK_dog_id, yard, other_pets, kids, female, size, FK_dogs_links_id) VALUES (5, true, false, true, true, "large", 5);
+
+INSERT INTO human_prefs (yard, other_pets, kids, small, medium, large) VALUES (false, false, true, false, true, true);
+INSERT INTO human_prefs (yard, other_pets, kids, small, medium, large) VALUES (true, true, true, true, true, true);
+INSERT INTO human_prefs (yard, other_pets, kids, small, medium, large) VALUES (false, false, false, true, false, false);
+INSERT INTO human_prefs (yard, other_pets, kids, small, medium, large) VALUES (true, false, true, false, true, false);
+INSERT INTO human_prefs (yard, other_pets, kids, small, medium, large) VALUES (true, false, false, false, false, true);
 
 INSERT INTO humans (name, password) VALUES ('John', 'blink182');
 INSERT INTO humans (name, password) VALUES ('Marshall', 'drink!c0ff33!');
@@ -33,12 +45,14 @@ INSERT INTO humans (name, password) VALUES ('Chaney', 'd0g54lyf3');
 INSERT INTO humans (name, password) VALUES ('David', 'cod3r');
 INSERT INTO humans (name, password) VALUES ('Stanley', 'ipromiseim18');
 
-INSERT INTO matches (human_id, dog_id) VALUES (1, 2);
-INSERT INTO matches (human_id, dog_id) VALUES (2, 14);
-INSERT INTO matches (human_id, dog_id) VALUES (3, 5);
-INSERT INTO matches (human_id, dog_id) VALUES (4, 7);
-INSERT INTO matches (human_id, dog_id) VALUES (5, 11);
+INSERT INTO matches (FK_human_id, FK_dog_id) VALUES (1, 2);
+INSERT INTO matches (FK_human_id, FK_dog_id) VALUES (2, 1);
+INSERT INTO matches (FK_human_id, FK_dog_id) VALUES (3, 5);
+INSERT INTO matches (FK_human_id, FK_dog_id) VALUES (4, 4);
+INSERT INTO matches (FK_human_id, FK_dog_id) VALUES (5, 3);
 
-SELECT * FROM dogs;
 SELECT * FROM humans;
+SELECT * FROM dogs;
+SELECT * FROM dogs_links;
+SELECT * FROM dogs_stats;
 SELECT * FROM matches;
